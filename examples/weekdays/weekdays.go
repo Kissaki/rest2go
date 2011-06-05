7ee1578438bab9d4acd868d9a7ce124f0bbdb59d
Kissaki <kissaki@gmx.de>
2011-06-06 01:05:17 +0200

    add new example: weekdays REST-server

---
+++ examples/weekdays/weekdays.go
@@ -0,0 +1,56 @@
+/*
+	Example REST server
+
+	When run can be opened in a webbrowser:
+	http://localhost:3000/wd/ for index, showing all entries as logic of the Index function specifies
+	http://localhost:3000/wd/We get full string for index We (other indices from map also possible)
+*/
+package main
+
+import (
+	"fmt"
+	"http"
+	"log"
+	"github.com/Kissaki/rest.go"
+)
+
+type Weekdays struct {
+	wd map[string]string
+}
+func (wd *Weekdays) Index(resp http.ResponseWriter) {
+	for i, v := range wd.wd {
+		fmt.Fprintf(resp, "%s: %s<br/>\n", i, v)
+	}
+}
+func (wd *Weekdays) Find(resp http.ResponseWriter, id string) {
+	if full, ok := wd.wd[id]; ok {
+		fmt.Fprintf(resp, full)
+	}
+}
+
+func main() {
+	serve()
+}
+
+func serve() {
+	log.Println("Starting Server")
+	address := "127.0.0.1:3000"
+	
+	var wd = new(Weekdays)
+	wd.wd = map[string]string{
+			"Mo":"Monday",
+			"Tu":"Tuesday",
+			"We": "Wednesday",
+			"Th": "Thursday",
+			"Fr": "Friday",
+			"Sa": "Saturday",
+			"Su": "Sunday",
+			
+		}
+	rest.Resource("wd", wd)
+
+	if err := http.ListenAndServe(address, nil); err != nil {
+		log.Fatalln(err)
+	}
+}
+
